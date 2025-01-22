c34cde6df069c3ff77f9764987d31bacaeb767ed
sarveshdakhore <sarveshd444@gmail.com>
2025-01-22 22:18:13 +0530

    fix: removed cors to check

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import env from 'dotenv';
 import bodyParser from 'body-parser';
 import router from './routes/routes.js';
-import cors from 'cors';
 import cron from 'node-cron';
 import deleteExpiredOtps from './utils/deleteExpiredOTPs_shedule.js';
 import session from 'express-session';
@@ -14,15 +13,6 @@ const app = express();
 const port = process.env.PORT || 8000; // Set default port to 8000
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({
-    origin: ["http://localhost:3000", "https://indiamun.org","https://indiamun-main-website-e3160.firebaseapp.com/","https://indiamun-main-website-e3160.web.app/"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true, // Allow credentials if needed
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-app.options('*', cors()); // Preflight response for all routes
 app.use(express.urlencoded({ extended: true }));
 
 // Configure session
@@ -32,17 +22,18 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-
 // Initialize Passport and restore authentication state, if any, from the session
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://indiamun.org");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials if needed
-    next();
-});
+
+// Remove CORS configuration to allow any origin
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+//     res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials if needed
+//     next();
+// });
 
 app.use(router);
 
@@ -51,7 +42,7 @@ cron.schedule('0 0 * * *', () => {
     deleteExpiredOtps();
 });
 
-// // Start the server
+// Start the server
 // app.listen(port, () => {
 //     console.log(`Server is running on http://localhost:${port}`);
 // });
