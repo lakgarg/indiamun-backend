9d37d2baf7041ead5c04fe755cbf1880ae079a51
Sarvesh Dakhore <sarveshpd444@gmail.com>
2025-02-28 21:35:55 +0530

    addec another fe link to cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8000; // Set default port to 8000
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors({
-    origin: "https://indiamun.org",
+    origin: ["https://indiamun.org", "https://www.indiamun.org"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
 }));
